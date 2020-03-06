23d6208f38967b677b92f0767778c8d7af8789b3
Steven Stevanus <stvn.org@gmail.com>
2020-03-05 23:28:44 -0600

    Standarized the ENV_VARIABLES of sequelize config.js

--- routes/models/config/config.js
+++ routes/models/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     password: process.env.DEV_DATABASE_PASSWORD,
     database: process.env.DEV_DATABASE_NAME,
     host: process.env.DEV_DATABASE_URL,
-    port: 5432,
+    port: process.env.DEV_DATABASE_PORT,
     dialect: 'postgres',
     dialectOptions: {
       bigNumberStrings: true
@@ -15,18 +15,18 @@ module.exports = {
     password: process.env.CI_DATABASE_PASSWORD,
     database: process.env.CI_DATABASE_NAME,
     host: process.env.CI_DATABASE_URL,
-    port: 5432,
+    port: process.env.CI_DATABASE_PORT,
     dialect: 'postgres',
     dialectOptions: {
       bigNumberStrings: true
     }
   },
   production: {
-    username: process.env.PROD_DATABASE_USERNAME,
-    password: process.env.PROD_DATABASE_PASSWORD,
-    database: process.env.PROD_DATABASE_NAME,
-    host: process.env.PROD_DATABASE_URL,
-    port: process.env.PROD_DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_URL,
+    port: process.env.DATABASE_PORT,
     dialect: 'postgres',
     dialectOptions: {
       bigNumberStrings: true
