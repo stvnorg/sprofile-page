f7bc836ee4eac5d171467638ae1d5e0a7d211980
Steven Stevanus <stvn.org@gmail.com>
2020-03-07 14:48:45 -0600

    Adding validation for the seeders command

--- routes/models/seeders/20200304050509-default-profile.js
+++ routes/models/seeders/20200304050509-default-profile.js
@@ -1,18 +1,25 @@
 'use strict'
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('profile', [{
-      id: 1,
-      page_title: 'Page Title',
-      image_path: '../images/image_profile.jpg',
-      name: 'FullName',
-      summary: 'Summary',
-      links_icons: '# fa-square;# fa-square;# fa-square;# fa-square',
-      about_me: 'A little bit about me',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {})
+  async up(queryInterface, Sequelize) {
+    const row = await queryInterface.rawSelect('profile', {
+      where: {
+        id: 1
+      }
+    }, ['id'])
+    if (!row) {
+      return queryInterface.bulkInsert('profile', [{
+        id: 1,
+        page_title: 'Page Title',
+        image_path: '../images/default.jpg',
+        name: 'FullName',
+        summary: 'Summary',
+        links_icons: 'http://localhost fa-square;http://localhost fa-square;http://localhost fa-square;http://localhost fa-square',
+        about_me: 'A little bit about me',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }])
+    }
   },
 
   down: (queryInterface, Sequelize) => {
--- routes/models/seeders/20200304051219-default-resume.js
+++ routes/models/seeders/20200304051219-default-resume.js
@@ -1,17 +1,24 @@
 'use strict'
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('resume', [{
-      id: 1,
-      job_title: 'Job Title',
-      company: 'Company',
-      period: 'Period',
-      location: 'Location',
-      achievements: 'Tasks, Responsibilities and Your Achievements',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {})
+  async up(queryInterface, Sequelize) {
+    const row = await queryInterface.rawSelect('resume', {
+      where: {
+        id: 1
+      }
+    }, ['id'])
+    if (!row) {
+      return queryInterface.bulkInsert('resume', [{
+        id: 1,
+        job_title: 'Job Title',
+        company: 'Company',
+        period: 'Period',
+        location: 'Location',
+        achievements: '<li>Tasks, Responsibilities and Your Achievements</li>',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }])
+    }
   },
 
   down: (queryInterface, Sequelize) => {
